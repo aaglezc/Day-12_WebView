522a81a079429a22011f2f5363e78f84ec799c6e
MacStudent <macstudent@126-iMac05.local>
2019-03-18 21:43:05 -0400

    WebKit

--- Day 12_WebView/ViewController.swift
+++ Day 12_WebView/ViewController.swift
@@ -7,14 +7,56 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
 
+
+class ViewController: UIViewController, WKNavigationDelegate {
+
+    @IBOutlet weak var webView1: WKWebView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        webView1.navigationDelegate = self
+        
+        webView1.allowsBackForwardNavigationGestures = true
+        
+        loadFromURL()
+        //loadFromString()
+        //loadFromFile()
+        
+        
+       
+    }
+    
+    func loadFromURL()
+    {
+        
+        let url = URL(string: "https://www.google.com")
+        let urlReq  = URLRequest(url: url!)
+        webView1.load(urlReq)
+        
     }
 
+    func loadFromFile()
+    {
+        
+        let localFilePath = Bundle.main.url(forResource: "home", withExtension: "html")
+        let myRequest = URLRequest(url: localFilePath!)
+        webView1.load(myRequest)
+        
+    }
+    
+    func loadFromString()
+    {
+        
+        let htmlStr = "<h1>Hello World</h1"
+        webView1.loadHTMLString(htmlStr, baseURL: nil)
+        
+    }
+    
 
 }
 
